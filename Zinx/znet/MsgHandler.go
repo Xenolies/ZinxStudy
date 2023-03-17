69587ab44af6fbc1c1f358134c688be73c3ee224
Xenolies <62866031+Xenolies@users.noreply.github.com>
2023-03-17 15:48:05 +0800

    ZinxStudy v0.6 多路由模式--消息管理模块定义和实现

---
+++ Zinx/znet/MsgHandler.go
@@ -0,0 +1,49 @@
+package znet
+
+import (
+	"ZinxDemo01/Zinx/ziface"
+	"fmt"
+	"strconv"
+)
+
+/*
+消息处理模块
+*/
+
+type MsgHandle struct {
+	// 存放每一个MsgID对应的处理方法
+	Apis map[uint32]ziface.IRouter
+}
+
+// NewMsgHandle 创建消息处理对象
+func NewMsgHandle() *MsgHandle {
+	return &MsgHandle{
+		Apis: make(map[uint32]ziface.IRouter),
+	}
+}
+
+// DoMsgHandler 调度  执行对应的Router消息处理方法
+func (mh *MsgHandle) DoMsgHandler(request ziface.IRequest) {
+	// 从 Request中找到 MsgID
+	handler, ok := mh.Apis[request.GetMsgID()]
+	if !ok {
+		fmt.Println("Api MsgID: ", request.GetMsgID(), " Not Found")
+		return
+	}
+	// 根据MsgID执行对应Router业务
+	handler.PreHandle(request)
+	handler.Handle(request)
+	handler.PostHandle(request)
+}
+
+// AddRouter 为消息添加具体的处理逻辑
+func (mh *MsgHandle) AddRouter(msgID uint32, router ziface.IRouter) {
+	// 判断当前msg绑定的Router是否存在
+	if _, ok := mh.Apis[msgID]; ok {
+		// ID 已经注册了
+		panic("Repeat API, MsgID: " + strconv.Itoa(int(msgID)))
+	}
+	// 添加Msg和API的绑定关系
+	mh.Apis[msgID] = router
+	fmt.Println("Add Api MsgID SUCCESS: ", msgID)
+}
